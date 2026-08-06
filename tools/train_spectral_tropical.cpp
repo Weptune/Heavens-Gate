e933a35a5e9c8022e23fffac6750ec7b0ec48d9a
Abhinav Krishna <abhinavkrishna047@gmail.com>
2026-08-06 06:10:34 +0530

    Expand rolling dataset buffer to 500,000 samples (~8 rounds memory)

--- tools/train_spectral_tropical.cpp
+++ tools/train_spectral_tropical.cpp
@@ -287,8 +287,8 @@ int main(int argc, char* argv[]) {
     // Append new round samples to rolling buffer
     dataset_buffer.insert(dataset_buffer.end(), dataset.begin(), dataset.end());
 
-    // Trim to most recent 300,000 samples
-    constexpr size_t MAX_BUFFER_SIZE = 300000;
+    // Trim to most recent 500,000 samples
+    constexpr size_t MAX_BUFFER_SIZE = 500000;
     if (dataset_buffer.size() > MAX_BUFFER_SIZE) {
         size_t excess = dataset_buffer.size() - MAX_BUFFER_SIZE;
         dataset_buffer.erase(dataset_buffer.begin(), dataset_buffer.begin() + excess);
